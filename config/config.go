2a04f6e8a4a8a39ec2eca5880c50134518dd2778
Teodor Savin <teodor.savin@workiva.com>
2023-06-20 22:58:11 +0200

    refactor

--- config/config.go
+++ config/config.go
@@ -2,19 +2,25 @@ package config
 
 import (
 	"database/sql"
+	"fmt"
+	"os"
+
 	_ "github.com/go-sql-driver/mysql"
 )
 
 func ConnectDB() *sql.DB {
 	dbDriver := "mysql"
-	dbUser := "user"
-	dbPass := "secret"
-	dbName := "ah_bonus"
-	hostName := "fullstack-mysql"
+	dbUser := os.Getenv("DB_USER")
+	dbPass := os.Getenv("DB_PASSWORD")
+	dbName := os.Getenv("DB_NAME")
+	hostName := os.Getenv("DB_HOST")
 
-	db, err := sql.Open(dbDriver, dbUser+":"+dbPass+"@tcp("+hostName+")/"+dbName)
+	dbURI := fmt.Sprintf("%s:%s@tcp(%s)/%s", dbUser, dbPass, hostName, dbName)
+
+	db, err := sql.Open(dbDriver, dbURI)
 	if err != nil {
 		panic(err.Error())
 	}
+
 	return db
 }
